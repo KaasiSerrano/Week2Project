1d4679b1c8af3b0e374ada20775558e5bc2a446b
kaasiSerrano <kaasi.serrano@gmail.com>
2019-03-10 13:36:20 -0400

    Layout appears and disappears landscape only

---
+++ app/src/main/java/com/example/framelayout/FavesFragmentBrain.java
@@ -0,0 +1,63 @@
+package com.example.framelayout;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class FavesFragmentBrain extends Fragment {
+
+TextView textTitle,textBody;
+LinearLayout layout;
+
+        @Override
+        public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.favesfragment, container, false);
+
+            layout = rootView.findViewById(R.id.linearLayoutAncho);
+
+            textTitle = rootView.findViewById(R.id.favesTitle);
+            textBody = rootView.findViewById(R.id.favesInformation);
+
+
+            return rootView;
+
+        }
+
+public void setTitle(String title)
+{
+    textTitle.setText(title);
+
+}
+
+    public void setBody(String title)
+    {
+        textBody.setText(title);
+
+    }
+public void changeFocus1()
+{
+    String isItVisable = String.valueOf(layout.getVisibility());
+
+    if(layout.getVisibility()==View.VISIBLE) {
+        layout.setVisibility(View.INVISIBLE);
+    }
+    else
+    {
+        layout.setVisibility(View.VISIBLE);
+
+    }
+    Log.d("faggot",isItVisable);
+}
+
+        }
+
+
--- app/src/main/java/com/example/framelayout/MainActivity.java
+++ app/src/main/java/com/example/framelayout/MainActivity.java
@@ -1,13 +1,83 @@
 package com.example.framelayout;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Switch;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    TextView t1, t2, t3, t4;
+
+    FavesFragmentBrain favesFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        favesFragment = (FavesFragmentBrain) getSupportFragmentManager().findFragmentById(R.id.fragment);
+
+        t1 = findViewById(R.id.textNum1);
+        t1.setOnClickListener(this);
+        t2 = findViewById(R.id.textNum2);
+        t2.setOnClickListener(this);
+        t3 = findViewById(R.id.textNum3);
+        t3.setOnClickListener(this);
+        t4 = findViewById(R.id.textNum4);
+        t4.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        favesFragment.changeFocus1();
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.textNum1: {
+                favesFragment.setTitle("This is Title 1");
+                favesFragment.setBody("THIS IS BODY 1");
+                favesFragment.changeFocus1();
+            }
+            break;
+
+            case R.id.textNum2: {
+                favesFragment.setTitle("This is Title 2");
+                favesFragment.setBody("THIS IS BODY 2");
+                favesFragment.changeFocus1();
+
+
+            }
+            break;
+
+            case R.id.textNum3: {
+                favesFragment.setTitle("This is Title 3");
+                favesFragment.setBody("THIS IS BODY 3");
+                favesFragment.changeFocus1();
+
+
+            }
+            break;
+
+            case R.id.textNum4: {
+                favesFragment.setTitle("This is Title 4");
+                favesFragment.setBody("THIS IS BODY 4");
+                favesFragment.changeFocus1();
+
+
+            }
+            break;
+
+        }
+
     }
 }
+
+
+
+
+
+
