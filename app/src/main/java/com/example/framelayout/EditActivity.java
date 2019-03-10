be0779a558dc0078a955687a2f92e93222e0b58a
Ivan Gallegos <gallegosivan9@gmail.com>
2019-03-10 18:09:43 -0400

    Finished v1 of 4 activities

--- app/src/main/java/com/example/framelayout/EditActivity.java
+++ app/src/main/java/com/example/framelayout/EditActivity.java
@@ -1,13 +1,78 @@
 package com.example.framelayout;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class EditActivity extends AppCompatActivity {
+
+    @BindView(R.id.tv_name_field)
+    EditText etNameField;
+    @BindView(R.id.tv_age)
+    EditText etAge;
+    @BindView(R.id.tv_gender)
+    EditText etGender;
+    @BindView(R.id.tv_occupation)
+    EditText etOccupation;
+    @BindView(R.id.tv_location)
+    EditText etLocation;
+    @BindView(R.id.tv_bio_field)
+    EditText etBioField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
+        ButterKnife.bind(this);
+        getData();
+    }
+
+    @OnClick(R.id.button_save)
+    public void onViewClicked() {
+        //Save data to Shared Preferences
+        saveData();
+        returnToProfile();
+    }
+
+    private void saveData() {
+        SharedPreferences sharedPreferences = getSharedPreferences("PROFILE", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("NAME", etNameField.getText().toString());
+        editor.putString("AGE", etAge.getText().toString());
+        editor.putString("GENDER", etGender.getText().toString());
+        editor.putString("OCCUPATION", etOccupation.getText().toString());
+        editor.putString("LOCATION", etLocation.getText().toString());
+        editor.putString("BIO", etBioField.getText().toString());
+        editor.apply();
+        Log.d("Main Activity 2", "Info Saved");
+    }
+    private void getData() {
+        SharedPreferences sharedPreferences = getSharedPreferences("PROFILE", Context.MODE_PRIVATE);
+        String name = sharedPreferences.getString("NAME", "");
+        String age = sharedPreferences.getString("AGE", "");
+        String gender = sharedPreferences.getString("GENDER", "");
+        String occupation = sharedPreferences.getString("OCCUPATION", "");
+        String location = sharedPreferences.getString("LOCATION", "");
+        String bio = sharedPreferences.getString("BIO", "");
+
+        etNameField.setText(name);
+        etAge.setText(age);
+        etGender.setText(gender);
+        etOccupation.setText(occupation);
+        etLocation.setText(location);
+        etBioField.setText(bio);
+    }
+    private void returnToProfile(){
+        Intent intent = new Intent(this,ProfileActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
--- app/src/main/java/com/example/framelayout/ProfileActivity.java
+++ app/src/main/java/com/example/framelayout/ProfileActivity.java
@@ -6,7 +6,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
+import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -14,11 +14,25 @@ import butterknife.OnClick;
 
 public class ProfileActivity extends AppCompatActivity {
 
+    @BindView(R.id.tv_name_field)
+    TextView tvNameField;
+    @BindView(R.id.tv_bio_field)
+    TextView tvBioField;
+    @BindView(R.id.tv_age)
+    TextView tvAge;
+    @BindView(R.id.tv_gender)
+    TextView tvGender;
+    @BindView(R.id.tv_occupation)
+    TextView tvOccupation;
+    @BindView(R.id.tv_location)
+    TextView tvLocation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         ButterKnife.bind(this);
+        getData();
     }
 
 
@@ -35,11 +49,32 @@ public class ProfileActivity extends AppCompatActivity {
                 finish();
                 break;
             case R.id.button_edit:
+                Intent intentEdit = new Intent(this, EditActivity.class);
+                startActivity(intentEdit);
                 break;
             case R.id.button_faves:
                 Intent intentFaves = new Intent(this, MainActivity.class);
                 startActivity(intentFaves);
                 break;
         }
+    }
+
+    private void getData() {
+        SharedPreferences sharedPreferences = getSharedPreferences("PROFILE", Context.MODE_PRIVATE);
+        String name = sharedPreferences.getString("NAME", "");
+        String age = sharedPreferences.getString("AGE", "");
+        String gender = sharedPreferences.getString("GENDER", "");
+        String occupation = sharedPreferences.getString("OCCUPATION", "");
+        String location = sharedPreferences.getString("LOCATION", "");
+        String bio = sharedPreferences.getString("BIO", "");
+
+        name = "Hello " + name;
+        bio = "BIO: " + bio;
+        tvNameField.setText(name);
+        tvAge.setText(age);
+        tvGender.setText(gender);
+        tvOccupation.setText(occupation);
+        tvLocation.setText(location);
+        tvBioField.setText(bio);
     }
 }
