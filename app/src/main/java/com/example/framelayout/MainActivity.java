bb21015a54d232e2755e7a30fa05ea6f217ed7e7
kaasiSerrano <kaasi.serrano@gmail.com>
2019-03-10 20:56:57 -0400

    Pictures added and language added for spanish

--- app/src/main/java/com/example/framelayout/MainActivity.java
+++ app/src/main/java/com/example/framelayout/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.framelayout;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Switch;
 import android.widget.TextView;
@@ -11,7 +12,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     TextView t1, t2, t3, t4;
     LinearLayout layout;
-
+ImageView profilePic;
     FavesFragmentBrain favesFragment;
 
     @Override
@@ -32,6 +33,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         t3.setOnClickListener(this);
         t4 = findViewById(R.id.textNum4);
         t4.setOnClickListener(this);
+        profilePic = findViewById(R.id.profilePic);
+
 
     }
 
@@ -54,45 +57,54 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (v.getId())
         {
             case R.id.textNum1: {
-                favesFragment.setTitle("This is Title 1");
-                favesFragment.setBody("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tincidunt, tellus sed feugiat tincidunt, justo risus auctor nunc, nec imperdiet tortor sem vitae ante. Aenean vitae tincidunt urna, gravida feugiat nibh. Morbi pretium magna arcu, ut volutpat nulla placerat nec. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam placerat lacus a consectetur lacinia. Aenean tempus, nibh nec commodo mattis, est diam sagittis turpis, ut accumsan urna lacus at urna. Ut pulvinar imperdiet elit a dictum.");
+                favesFragment.setTitle(v.getResources().getString(R.string.sample_text_1));
+                favesFragment.setBody(v.getResources().getString(R.string.jessicarabbit));
+
                 if (favesFragment.isorientationLandscape() == true) {
                     favesFragment.makeVisiable();
                 }else {
                     favesFragment.changeFocus1();
                 }
+                profilePic.setBackgroundResource(R.drawable.jessicarabbit);
+
             }break;
 
             case R.id.textNum2: {
-                favesFragment.setTitle("This is Title 2");
-                favesFragment.setBody("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tincidunt, tellus sed feugiat tincidunt, justo risus auctor nunc, nec imperdiet tortor sem vitae ante. Aenean vitae tincidunt urna, gravida feugiat nibh. Morbi pretium magna arcu, ut volutpat nulla placerat nec. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam placerat lacus a consectetur lacinia. Aenean tempus, nibh nec commodo mattis, est diam sagittis turpis, ut accumsan urna lacus at urna. Ut pulvinar imperdiet elit a dictum.");
+                favesFragment.setTitle(v.getResources().getString(R.string.sample_text_2));
+                favesFragment.setBody(v.getResources().getString(R.string.ladygaga));
                 if (favesFragment.isorientationLandscape() == true) {
                     favesFragment.makeVisiable();
                 }else {
                     favesFragment.changeFocus1();
                 }
+                profilePic.setBackgroundResource(R.drawable.ladygaga);
+
                 break;
             }
             case R.id.textNum3: {
-                favesFragment.setTitle("This is Title 3");
-                favesFragment.setBody("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tincidunt, tellus sed feugiat tincidunt, justo risus auctor nunc, nec imperdiet tortor sem vitae ante. Aenean vitae tincidunt urna, gravida feugiat nibh. Morbi pretium magna arcu, ut volutpat nulla placerat nec. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam placerat lacus a consectetur lacinia. Aenean tempus, nibh nec commodo mattis, est diam sagittis turpis, ut accumsan urna lacus at urna. Ut pulvinar imperdiet elit a dictum.");
+                favesFragment.setTitle(v.getResources().getString(R.string.sample_text_3));
+                favesFragment.setBody(v.getResources().getString(R.string.dialupa));
                 if(favesFragment.isorientationLandscape()==true) {
                     favesFragment.makeVisiable();
                 }else {
                     favesFragment.changeFocus1();
                 }
+                profilePic.setBackgroundResource(R.drawable.dialupa);
+
             }
             break;
 
             case R.id.textNum4: {
-                favesFragment.setTitle("This is Title 4");
-                favesFragment.setBody("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tincidunt, tellus sed feugiat tincidunt, justo risus auctor nunc, nec imperdiet tortor sem vitae ante. Aenean vitae tincidunt urna, gravida feugiat nibh. Morbi pretium magna arcu, ut volutpat nulla placerat nec. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam placerat lacus a consectetur lacinia. Aenean tempus, nibh nec commodo mattis, est diam sagittis turpis, ut accumsan urna lacus at urna. Ut pulvinar imperdiet elit a dictum.");
+                favesFragment.setTitle(v.getResources().getString(R.string.sample_text_4));
+                favesFragment.setBody(v.getResources().getString(R.string.rileyreid));
                 if(favesFragment.isorientationLandscape()==true) {
                     favesFragment.makeVisiable();
                 }else {
                     favesFragment.changeFocus1();
                 }
-                }break;
+                profilePic.setBackgroundResource(R.drawable.reilyreid);
+
+            }break;
 
         }
 
